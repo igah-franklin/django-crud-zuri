563d95be10b6e6234654bbda33a7dbda26336352
udodirim.com <igahfranklyn@gmail.com>
2022-06-27 00:38:52 +0200

    added urls

--- i4g001499uyx/blog/views.py
+++ i4g001499uyx/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
 # Create your views here.
+from blog.models import  Post
+from django.views.generic import ListView,CreateView ,DetailView, UpdateView, DeleteView
+
+
+class PostListView(ListView):
+    pass
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all_'
+    success_url = reverse_lazy('blog:all')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all_'
+    success_url = reverse_lazy('blog:all')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all_'
+    success_url = reverse_lazy('blog:all')
+
